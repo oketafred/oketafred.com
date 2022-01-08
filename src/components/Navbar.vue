383edf780d38ec4ed9db5ffb1593b2aac4df1f0c
Oketa Fred <oketafred@gmail.com>
2022-01-09 01:08:43 +0300

    Adds responsive navigation menu

---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="bg-gray-900 text-gray-100 py-3.5 xl:px-52 sm:px-30 px-6 shadow md:flex justify-between items-center">
+    <div class="flex items-center cursor-pointer">
+      <span class=" text-green-500 text-xl mr-1">
+        Icon
+      </span>
+      <h1 class=" text-xl">Designer</h1>
+    </div>
+    <span @click="menuOpen()" class="absolute md:hidden right-6 top-1.5 cursor-pointer text-4xl">
+      icon
+    </span>
+    <ul class="md:flex md:items-center md:px-0 px-10 md:pb-0 pb-10 md:static absolute left-0 bg-gray-900 md:w-auto w-full top-14 duration-700 ease-in" :class="[open ? 'hidden' : 'block']">
+      <li class="-mx-4 md:mx-4 md:my-0 my-6" v-for="link in links" :key="link.name">
+        <a :href="link.url" class="text-xl hover:text-green-500">{{ link.name }}</a>
+      </li>
+      <li class="-ml-4 md:ml-4">
+        <button class=" bg-green-400 hover:bg-green-500 duration-300 text-sm font-semibold text-white rounded py-1.5 px-4">Download Resume</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      open: false,
+      links: [
+        { name: "Home", url: "#" },
+        { name: "Services", url: "#" },
+        { name: "About", url: "#" },
+        { name: "Contact", url: "#" }
+      ]
+    }
+  },
+  methods: {
+    menuOpen() {
+      this.open = !this.open;
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
