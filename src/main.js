ce62d1a66d47eb67dd64021126d289be4cb8b78b
Oketa Fred <oketafred@gmail.com>
2022-01-26 17:25:23 +0300

    Adds google analytics

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router/index';
+import VueAnalytics from 'vue-analytics';
 
 import '@fortawesome/fontawesome-free/js/all.js';
 
 Vue.config.productionTip = false
 import '../assets/css/styles.css';
+
+// Configuration VueAnalytics
+Vue.use(VueAnalytics, {
+  id: 'UA-161194796-1',
+  router
+});
 
 new Vue({
   render: h => h(App),
