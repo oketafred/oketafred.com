c335ca4abf190ef1d11f6f9979338e6bd838b7f4
Shift <shift@laravelshift.com>
2022-02-09 17:05:00 +0000

    Streamline Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,1 @@
-module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.js',
-    './src/**/*.vue',
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+module.exports = { purge: ['./src/**/*.html', './src/**/*.js', './src/**/*.vue'] }
